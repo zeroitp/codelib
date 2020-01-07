29650c7898fbba56bdb4923a426f2159761b08ee
Tai Vo <zeroitp@gmail.com>
2020-01-07 22:19:54 +0700

    done for setup gulp and auto publish - result: fail on both gulp and auto publish - reason: vs2019 do not support

--- Source/CodeLib/CodeLib/gulpfile.js
+++ Source/CodeLib/CodeLib/gulpfile.js
@@ -1,4 +1,4 @@
-﻿/// <binding AfterBuild='default' />
+﻿/// <binding />
 /*
 This file is the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
@@ -9,23 +9,18 @@ Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var run = require('gulp-run');
+
 var jsSources = ['wwwroot/scripts/*.js'];
 var webConfig = ['obj/Debug/netcoreapp3.0/PubTmp/Out/web.config'];
-var jsonFile = ['package.json',
-    'bin/Debug/netcoreapp3.0/CodeLib.runtimeconfig.json',
-    'bin/Debug/netcoreapp3.0/CodeLib.deps.json',
-    'appsettings.json',
-    'appsettings.Development.json'
-];
+var jsonFile = ['bin/Debug/netcoreapp3.0/*.json'];
 var wwwroot = ['wwwroot'];
-var dllFile = ['bin/Debug/netcoreapp3.0/CodeLib.dll',
-    'bin/Debug/netcoreapp3.0/CodeLib.exe',
-    'bin/Debug/netcoreapp3.0/CodeLib.pdb',
-    'bin/Debug/netcoreapp3.0/CodeLib.Views.dll',
-    'bin/Debug/netcoreapp3.0/CodeLib.Views.pdb'
+var dllFile = ['bin/Debug/netcoreapp3.0/*.dll',
+    'bin/Debug/netcoreapp3.0/*.pdb',
+    'bin/Debug/netcoreapp3.0/*.exe'
 ];
 
-var desFolder = 'D:/Project Web/CodeLibrary/test/';
+var desFolder = 'D:/Project Web/CodeLibrary/publish/';
 
 gulp.task('copy', gulp.series(function (done) {
     gulp.src(webConfig).pipe(gulp.dest(desFolder));
